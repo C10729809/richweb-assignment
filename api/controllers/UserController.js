2deac91c5a54212838cde536e115400c6fa439c2
C10729809 <glen.holmes@student.dit.ie>
2013-12-11 14:18:25 +0000

    Commit 6

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -16,18 +16,54 @@
  */
 
 module.exports = {
-	'new': function(req, res){
+	'new' : function(req, res){
 		res.view()
 	},
 	
-	create: function(req, res, next){
+	create : function(req, res, next){
 		User.create(req.params.all(), function userCreated(err, user){
 			if(err){
 				console.log(err);
 				return res.redirect("/user/new");
-			} else {
-				res.json(user);
 			}
+			res.redirect("/user/show/"+user.id);
 		});	
+	},
+	
+	show: function (req, res, next){
+		User.findOne(req.param('id'), function found(err, user){
+			if(err) {
+				console.log(err);
+			}
+			if(!user){
+				return res.redirect("/static/");
+			}
+			res.view({
+				user: user
+			});		
+		});
+	},
+
+	edit: function (req, res, next){
+		User.findOne(req.param('id'), function found(err, user){
+			if(err) {
+				console.log(err);
+			}
+			if(!user){
+				return res.redirect("/");
+			}
+			res.view({
+				user: user
+			});		
+		});
+	},
+
+	update: function(req, res, next){
+		User.update(req.param('id'), req.params.all(), function userUpdate(err){
+			if(err){
+				return res.redirect("/user/edit/" + req.param('id'));
+			}
+			res.redirect("user/show/" + req.param('id'));
+		});
 	}
 };
