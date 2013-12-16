7c83783c24228465e872b752c0914db430feeb17
kyleotoole <kyle-360@hotmail.com>
2013-12-16 22:11:40 +0000

    kyle

--- assets/linker/js/slideshow.js
+++ assets/linker/js/slideshow.js
@@ -1,20 +1,21 @@
 var images = ["../images/hgbanner.jpg","../images/grav_image.jpg","../images/thecouncelor.jpg"];
-
 var imgNum = 0;
 var imgLength = images.length -1;
 var intv;
-
+//////////////////////////////////////////////////////////////////////////
+/// Slide show code for fading in/out the banner and changing image //////
 $( document ).ready(function() {
+     //start with random image
      imgNum = Math.floor((Math.random()*imgLength)+1);
      document.getElementById('bannerImg').src = images[imgNum];    
 });
 
 $(function(){
+    //call showImage every 5 seconds
     intv = setInterval(function(){showImage()},5000);  
-    //clearInterval(intv); 
     return false;
 });
-
+//showImage is responsable for fading and changing the image 
 function showImage()
 {
    if (imgNum > imgLength) {
@@ -29,17 +30,23 @@ function showImage()
     imgNum++;
   }); 
 }
+/// End of slideshow code   /////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
+
+//connect to socketio and get the list of messages from the database
+//and populate the message list
 var socket = io.connect('http://localhost:1337');
 socket.on('connect', function(){
   socket.request('/chat',{}, function(result){
     for(var i in result){
-	  $("#msgList").append('<li>'+result[i].message+'<li>');
+	  $("#msgList").prepend('<li>'+result[i].message+'<li>');
     }
 
   });
 });
 
+//listen for message event and append the new message to message list
 socket.on('message', function(msg){
    //alert('message received: ');
    //$("#msgList").prepend('<li>'+msg.data.message+'<li>');
